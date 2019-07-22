a226bb36018af93e3a64eaa7343ed468bdcb8fbc
mjalbert13 <mjalbert13@gmail.com>
2019-07-22 14:57:38 -0400

    setting up local variables

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 var express = require("express");
-var exphbs = require("express-handlebars");
+//var session = require("express-session");
+var passport = require("passport"),
+  LocalStrategy = require('passport-local').Strategy;
 
 var db = require("./models");
 
@@ -11,19 +13,19 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
-
-// Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
+// app.use(
+//   session({
+//     secret: "Sheetz13!",
+//     resave: false,
+//     saveUninitialized: true
+//   })
+// );
+app.use(passport.initialize());
 
 // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
+require("./routes/loginRoutes")(app);
 
 var syncOptions = { force: false };
 
